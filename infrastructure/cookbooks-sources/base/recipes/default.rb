ddc9c6dcd60f78341a19e824ae926439c37f049a
David Laing <david@davidlaing.com>
2012-08-29 17:19:05 +0100

    Ensure members of admin group under Ubuntu can sudo without a password

--- infrastructure/cookbooks-sources/base/recipes/default.rb
+++ infrastructure/cookbooks-sources/base/recipes/default.rb
@@ -11,3 +11,11 @@
 node['users'] = ['mrdavidlaing', 'sopel']
 
 include_recipe "user::data_bag"
+
+sudo "group_admin_NOPASSWD" do
+  group "admin"
+  commands ["ALL"] # array of commands, will be .join(",")
+  host "ALL"
+  nopasswd true # true prepends the runas_spec with NOPASSWD
+  not_if { node['platform'].include? "windows" }
+end
