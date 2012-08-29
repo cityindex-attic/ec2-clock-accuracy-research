770150c127503b286e8bcb2a122dd94d3db25491
David Laing <david@davidlaing.com>
2012-08-29 18:34:55 +0100

    Make users in the groups::super_admins data_bag local admins on Ubuntu & Windows machines

---
+++ infrastructure/cookbooks-sources/base/attributes/default.rb
@@ -0,0 +1,2 @@
+default['group']['data_bag_name']          = "groups"
+default['group']['super_admins_group_name'] = "super_admins"
--- infrastructure/cookbooks-sources/base/recipes/default.rb
+++ infrastructure/cookbooks-sources/base/recipes/default.rb
@@ -7,10 +7,36 @@
 # Apache v2
 #
 
+group_bag = node['group']['data_bag_name']
+user_bag = node['user']['data_bag_name']
+super_admins = data_bag_item(group_bag, node['group']['super_admins_group_name'])['users']
 
-node['users'] = ['mrdavidlaing', 'sopel']
+super_admins.each do |super_admin_name|
+	u = data_bag_item(user_bag, super_admin_name.gsub(/[.]/, '-'))
+	username = u['username'] || u['id']
 
-include_recipe "user::data_bag"
+	user_account username do
+		%w{comment uid gid home shell password system_user manage_home create_group
+		    ssh_keys ssh_keygen}.each do |attr|
+		  send(attr, u[attr]) if u[attr]
+		end
+		action u['action'].to_sym if u['action']
+	end
+
+	if platform?(%w{debian ubuntu})
+		group "admin" do
+		    members username
+		    append true
+		end
+	end
+
+	if platform? 'windows'
+		group "Administrators" do
+		    members username
+		    append true
+		end
+	end
+end
 
 sudo "group_admin_NOPASSWD" do
   group "admin"
