b3a691ad5a9fc6bee9d319070585465665e5e71b
David Laing <david@davidlaing.com>
2012-09-11 15:11:26 +0100

    Set random password for windows NTP user

--- infrastructure/cookbooks-sources/windows_ntpd/attributes/default.rb
+++ infrastructure/cookbooks-sources/windows_ntpd/attributes/default.rb
@@ -1,2 +1,7 @@
+CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
+def random_password(length=10)
+   CHARS.sort_by { rand }.join[0...length]
+end
+
 default["windows_ntpd"]["service_user"] = "ntp"
-default["windows_ntpd"]["service_password"] = "override_me" #This must be overridden at the role or node level
+default["windows_ntpd"]["service_password"] = random_password(12)
